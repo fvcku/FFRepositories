6d7b5926dfd09de51619fb7e02f5b28260ee3683
imissu <Kingweb@imissdeMac-mini.local>
2018-07-11 16:40:55 +0800

    log
     Committer: fvcku <imissingu@aliyun.com>

---
+++ CRProtocolManager/CRProtocolManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRProtocolManager
+//
+//  Created by imissu on 2018/7/11.
+//  Copyright © 2018年 imissu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
